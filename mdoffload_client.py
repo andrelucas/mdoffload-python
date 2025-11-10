c843e5a6f04e48db1b9687d1fa15da666db726e8
André Lucas <andre@ae-35.com>
2025-11-10 15:14:50 +0000

    Initial commit, MVP client and server.

---
+++ mdoffload_client.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python3
+"""
+Simple test client for the MDOffload gRPC service.
+"""
+
+import argparse
+import base64
+import coloredlogs
+from google.rpc import code_pb2
+from google.rpc import error_details_pb2
+from google.rpc import status_pb2
+from google.protobuf.json_format import MessageToJson
+from google.protobuf.timestamp_pb2 import Timestamp
+import grpc
+from grpc_status import rpc_status
+import logging
+import os
+import sys
+
+from mdoffload.v1 import mdoffload_pb2_grpc
+from mdoffload.v1 import mdoffload_pb2
+
+
+def gather_attributes(args) -> tuple[dict[str, str], list[str]]:
+    attributes_to_add: dict[str, str] = {}
+    attributes_to_delete: list[str] = []
+    if args.add_attribute:
+        for attr in args.add_attribute:
+            k, v = attr.split("=", 1)
+            attributes_to_add[k] = v
+    if args.delete_attribute:
+        for attr in args.delete_attribute:
+            attributes_to_delete.append(attr)
+    return attributes_to_add, attributes_to_delete
+
+
+def get_bucket_attributes(stub, args):
+    request = mdoffload_pb2.GetBucketAttributesRequest(
+        user_id=args.user_id,
+        bucket_id=args.bucket_id if args.bucket_id else "",
+        bucket_name=args.bucket_name if args.bucket_name else "",
+    )
+    logging.debug(f"get_bucket_attributes request: {request}")
+    response = stub.GetBucketAttributes(request)
+    logging.debug(f"get_bucket_attributes response: {response}")
+    print(MessageToJson(response))
+    return True
+
+
+def set_bucket_attributes(stub, args):
+    attributes_to_add, attributes_to_delete = gather_attributes(args)
+
+    request = mdoffload_pb2.SetBucketAttributesRequest(
+        user_id=args.user_id,
+        bucket_id=args.bucket_id if args.bucket_id else "",
+        bucket_name=args.bucket_name if args.bucket_name else "",
+        attributes_to_add=attributes_to_add,
+        attributes_to_delete=attributes_to_delete
+    )
+    logging.debug(f"set_bucket_attributes request: {request}")
+    response = stub.SetBucketAttributes(request)
+    logging.debug(f"set_bucket_attributes response: {response}")
+    print(MessageToJson(response))
+    return True
+
+
+def get_object_attributes(stub, args):
+    request = mdoffload_pb2.GetObjectAttributesRequest(
+        user_id=args.user_id,
+        bucket_id=args.bucket_id if args.bucket_id else "",
+        bucket_name=args.bucket_name if args.bucket_name else "",
+        object_key=args.object_key if args.object_key else "",
+        object_instance_id=args.instance_id if args.instance_id else "",
+    )
+    logging.debug(f"get_object_attributes request: {request}")
+    response = stub.GetObjectAttributes(request)
+    logging.debug(f"get_object_attributes response: {response}")
+    print(MessageToJson(response))
+    return True
+
+
+def set_object_attributes(stub, args):
+    attributes_to_add, attributes_to_delete = gather_attributes(args)
+
+    request = mdoffload_pb2.SetObjectAttributesRequest(
+        user_id=args.user_id,
+        bucket_id=args.bucket_id if args.bucket_id else "",
+        bucket_name=args.bucket_name if args.bucket_name else "",
+        object_key=args.object_key,
+        object_instance_id=args.instance_id,
+        attributes_to_add=attributes_to_add,
+        attributes_to_delete=attributes_to_delete,
+    )
+    logging.debug(f"set_object_attributes request: {request}")
+    response = stub.SetObjectAttributes(request)
+    logging.debug(f"set_object_attributes response: {response}")
+    print(MessageToJson(response))
+    return True
+
+
+def issue(channel, args):
+    """
+    Issue the RPC. Factored out so we can use different types of channel.
+    """
+    stub = mdoffload_pb2_grpc.MDOffloadServiceStub(channel)
+
+    try:
+        if args.command == "get-bucket-attributes":
+            return get_bucket_attributes(stub, args)
+        elif args.command == "set-bucket-attributes":
+            return set_bucket_attributes(stub, args)
+        elif args.command == "get-object-attributes":
+            return get_object_attributes(stub, args)
+        elif args.command == "set-object-attributes":
+            return set_object_attributes(stub, args)
+        else:
+            logging.error(f"Unknown command '{args.command}'")
+            sys.exit(2)
+    except grpc.RpcError as e:
+        logging.error(f"gRPC error: {e.code()} - {e.details()}")
+        if e.code() == grpc.StatusCode.UNKNOWN:
+            status = rpc_status.from_call(e)
+            if status is not None:
+                for detail in status.details:
+                    if detail.Is(error_details_pb2.DebugInfo.DESCRIPTOR):
+                        debug_info = error_details_pb2.DebugInfo()
+                        detail.Unpack(debug_info)
+                        logging.error(f"Debug info: {debug_info.stack_entries}")
+        return False
+
+
+def _load_credential_from_file(filepath):
+    """https://github.com/grpc/grpc/blob/master/examples/python/auth/_credentials.py"""
+    real_path = os.path.join(os.path.dirname(__file__), filepath)
+    with open(real_path, "rb") as f:
+        return f.read()
+
+
+def main(argv):
+    p = argparse.ArgumentParser(description="AuthService client")
+    p.add_argument("command", help="command to run",
+                   choices=["get-bucket-attributes", "set-bucket-attributes",
+                            "get-object-attributes", "set-object-attributes",])
+    p.add_argument("-b", "--bucket-name", help="bucket name", default="")
+    p.add_argument("--bucket-id", help="bucket id", default="")
+    p.add_argument("-k", "--object-key",
+                   help="object key to authorize", default="")
+    p.add_argument("-i", "--instance-id", help="instance id", default="")
+    p.add_argument("-u", "--user-id", help="user id", default="testid")
+    p.add_argument("-A", "--add-attribute",
+                   help="set a bucket attribute (k=v)", action="append")
+    p.add_argument("-D", "--delete-attribute",
+                   help="set an object attribute (k=v)", action="append")
+    p.add_argument("-t", "--tls", help="connect to the server using TLS", action="store_true"
+                   )
+    p.add_argument(
+        "--uri", help="server uri (will override address and port!)")
+    p.add_argument("-a", "--address", help="server address",
+                   default="127.0.0.1")
+    p.add_argument("-p", "--port", type=int, default=8004,
+                   help="server listen port")
+    p.add_argument("-v", "--verbose", action="store_true")
+    ptls = p.add_argument_group("TLS arguments")
+    ptls.add_argument("--ca-cert", help="CA certificate file")
+    ptls.add_argument(
+        "--client-cert", help="client certificate file (NOT YET USED)")
+    ptls.add_argument("--client-key", help="client key file (NOT YET USED)")
+
+    args = p.parse_args(argv)
+    if not args.command:
+        p.usage()
+        sys.exit(1)
+
+    if args.verbose:
+        coloredlogs.install(level=logging.DEBUG)
+    else:
+        coloredlogs.install(level=logging.INFO)
+
+    if args.tls:
+        if not args.ca_cert:
+            logging.error("TLS requires a CA certificate")
+            sys.exit(1)
+
+    # Set up a channel string first.
+    server_address = f"dns:{args.address}:{args.port}"
+    if args.uri:
+        server_address = args.uri
+    logging.debug(f"using server_address {server_address}")
+    success = False
+
+    if args.tls:
+        root_crt = _load_credential_from_file(args.ca_cert)
+        channel_credential = grpc.ssl_channel_credentials(root_crt)
+        with grpc.secure_channel(server_address, channel_credential) as channel:
+            success = issue(channel, args)
+    else:
+        with grpc.insecure_channel(server_address) as channel:
+            success = issue(channel, args)
+
+    if success:
+        logging.info("Success")
+        sys.exit(0)
+    else:
+        logging.error("RPC failed")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ mdoffload_server.py
@@ -0,0 +1,408 @@
+#!/usr/bin/env python3
+"""
+Minimal HTTP server implementing the MDOffload protocol.
+
+Usage::
+    ./mdoffload_server.py [-v] [<port>]
+"""
+
+import argparse
+import base64
+from collections.abc import Generator
+import coloredlogs
+from concurrent import futures
+from google.protobuf import any_pb2
+from google.protobuf.json_format import MessageToJson
+from google.rpc import code_pb2
+from google.rpc import error_details_pb2
+from google.rpc import status_pb2
+import grpc
+from grpc_status import rpc_status
+import logging
+import re
+import os
+import sys
+import json
+
+from mdoffload.v1 import mdoffload_pb2_grpc
+from mdoffload.v1 import mdoffload_pb2
+
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    # ConsoleSpanExporter,
+)
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.instrumentation.grpc import GrpcInstrumentorServer
+
+resource = Resource(
+    attributes={
+        SERVICE_NAME: "mdoffload_server",
+    }
+)
+
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(
+    OTLPSpanExporter(endpoint="localhost:4317", insecure=True)
+)
+# processor = BatchSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(processor)
+
+# Sets the global default tracer provider
+trace.set_tracer_provider(provider)
+
+# Creates a tracer from the global tracer provider
+tracer = trace.get_tracer(__name__)
+
+grpc_server_instrumentor = GrpcInstrumentorServer()
+grpc_server_instrumentor.instrument()
+
+
+class Attributes:
+    def __init__(self):
+        self.attrs: dict[str, str] = {}
+
+    def set(self, key: str, value: str) -> None:
+        self.attrs[key] = value
+
+    def get(self, key: str, create_if_missing: bool = False) -> str:
+        if key not in self.attrs:
+            if not create_if_missing:
+                raise KeyError(f"Attribute {key} not found")
+            self.attrs[key] = ""
+        return self.attrs[key]
+
+    def update(self, attributes_to_add: dict[str, str], attributes_to_delete: list[str]) -> None:
+        for k, v in attributes_to_add.items():
+            logging.debug(f"Setting attribute {k}={v}")
+            self.attrs[k] = v
+        for k in attributes_to_delete:
+            if '=' in k:
+                logging.warning(f"Attribute delete key '{k}' contains '=', may be in error")
+            if k in self.attrs:
+                logging.debug(f"Deleting attribute {k}")
+                del(self.attrs[k])
+            else:
+                logging.debug(f"Attribute {k} not found, cannot delete")
+
+    def list(self) -> Generator[tuple[str, str]]:
+        return ((k, v) for k, v in self.attrs.items())
+
+
+class ObjectKey:
+    def __init__(self, key_name: str, instance_id: str = ""):
+        if key_name == "" and instance_id == "":
+            raise ValueError(
+                "Object key name and instance id cannot both be empty")
+        self.key = key_name
+        self.instance_id = instance_id
+        self.attributes = Attributes()
+
+    def attrs(self) -> Attributes:
+        return self.attributes
+
+    def __hash__(self):
+        return hash((self.key, self.instance_id))
+
+    def has_instance_id(self) -> bool:
+        return self.instance_id != ""
+
+    def __eq__(self, other):
+        if not isinstance(other, ObjectKey):
+            return False
+        return self.key == other.key and self.instance_id == other.instance_id
+
+    def __str__(self):
+        return f"ObjectKey[{self.key}/{self.instance_id}]"
+
+
+class Object:
+    def __init__(self, key: ObjectKey, bucket: 'Bucket'):
+        self.key = key
+        self.bucket = bucket
+
+    def attrs(self) -> Attributes:
+        return self.key.attributes
+
+
+class Bucket:
+    def __init__(self, name: str):
+        if not name:
+            raise ValueError("Bucket name cannot be empty")
+        if not re.match(r"^[a-z0-9.-]{3,63}$", name):
+            raise ValueError(f"Invalid bucket name: {name}")
+        self.name = name
+        self.objects: dict[ObjectKey, Object] = {}
+        self.attributes = Attributes()
+
+    def attrs(self) -> Attributes:
+        return self.attributes
+
+    def get_object(self, key: ObjectKey, create_if_missing: bool = False) -> Object:
+        if key not in self.objects:
+            if not create_if_missing:
+                raise KeyError(
+                    f"Object key {key} not found in bucket {self.name}")
+            logging.debug(f"Creating object {key} in bucket {self.name}")
+            self.objects[key] = Object(key, self)
+        return self.objects[key]
+
+    def delete_object(self, key: ObjectKey) -> None:
+        if key not in self.objects:
+            raise KeyError(f"Object key {key} not found")
+        del self.objects[key]
+
+    def list(self) -> Generator[tuple[ObjectKey, Object]]:
+        return ((k, v) for k, v in self.objects.items())
+
+
+class Store:
+    def __init__(self):
+        self.buckets: dict[str, Bucket] = {}
+        self.buckets_id_by_name: dict[str, str] = {}
+
+    def get_bucket_by_id(self, bucket_id: str, create_if_missing: bool = False) -> Bucket:
+        if bucket_id not in self.buckets:
+            if not create_if_missing:
+                raise KeyError(f"Bucket ID {bucket_id} not found")
+            self.buckets[bucket_id] = Bucket()
+        return self.buckets[bucket_id]
+
+    def get_bucket_by_name(self, bucket_name: str, create_if_missing: bool = False) -> Bucket:
+        if bucket_name not in self.buckets_id_by_name:
+            if not create_if_missing:
+                raise KeyError(f"Bucket name {bucket_name} not found")
+            bucket_id = self.create_bucket(bucket_name)
+        else:
+            bucket_id = self.buckets_id_by_name[bucket_name]
+        return self.get_bucket_by_id(bucket_id, create_if_missing=create_if_missing)
+
+    def create_bucket(self, bucket_name: str) -> str:
+        if bucket_name in self.buckets_id_by_name:
+            return self.buckets_id_by_name[bucket_name]
+        bucket_id = f"bucket-{len(self.buckets)+1}"
+        self.buckets_id_by_name[bucket_name] = bucket_id
+        self.buckets[bucket_id] = Bucket(bucket_name)
+        return bucket_id
+
+
+class MDOffloadServer(mdoffload_pb2_grpc.MDOffloadServiceServicer):
+    """MDOffload gRPC server implementation."""
+
+    def __init__(self, args: argparse.Namespace):
+        super().__init__()
+        self.store = Store()
+        self.args = args
+
+    def get_bucket(self, bucket_name: str, bucket_id: str, create_if_missing: bool) -> Bucket | None:
+        if bucket_id:
+            try:
+                return self.store.get_bucket_by_id(bucket_id)
+            except KeyError:
+                return None
+        elif bucket_name:
+            try:
+                return self.store.get_bucket_by_name(bucket_name, create_if_missing=create_if_missing)
+            except KeyError:
+                return None
+        return None
+
+    def get_object(self, bucket: Bucket, object_key: str, instance_id: str, create_if_missing: bool = False) -> Object | None:
+        # Allow ValueError to propagate up - this is an protocol error we should return.
+        key = ObjectKey(object_key, instance_id)
+        try:
+            return bucket.get_object(key, create_if_missing=create_if_missing)
+        except KeyError:
+            return None
+
+    def GetBucketAttributes(self, request, context):
+        with tracer.start_as_current_span("GetBucketAttributes"):
+            logging.debug(f"GetBucketAttributes request: [{request}]")
+
+            bucket_id = self.get_bucket(
+                request.bucket_name, request.bucket_id, True)
+            if bucket_id is None:
+                context.set_code(grpc.StatusCode.NOT_FOUND)
+                context.set_details("Bucket not found")
+                logging.error(
+                    f"Bucket not found: {request.bucket_name} / {request.bucket_id}: "
+                    f"{context.code()} {context.details().decode('UTF-8')}")
+                return mdoffload_pb2.GetBucketAttributesResponse()
+
+            response = mdoffload_pb2.GetBucketAttributesResponse(
+                attributes=bucket_id.attrs().list()
+            )
+
+            logging.debug(f"GetBucketAttributes response: {response}")
+            return response
+
+    def SetBucketAttributes(self, request, context):
+        with tracer.start_as_current_span("SetBucketAttributes"):
+            logging.debug(f"SetBucketAttributes request: [{request}]")
+
+            bucket_id = self.get_bucket(
+                request.bucket_name, request.bucket_id, True)
+            if bucket_id is None:
+                context.set_code(grpc.StatusCode.NOT_FOUND)
+                context.set_details("Bucket not found")
+                logging.error(f"Bucket not found: {request.bucket_name} / {request.bucket_id}: "
+                              f"{context.code()} {context.details().decode('UTF-8')}")
+                return mdoffload_pb2.SetBucketAttributesResponse()
+
+            bucket_id.attrs().update(
+                request.attributes_to_add, request.attributes_to_delete
+            )
+            logging.debug(
+                f"Updated bucket attributes: {','.join([f'{k}={v}' for k,v in bucket_id.attrs().list()])}")
+
+            response = mdoffload_pb2.SetBucketAttributesResponse()
+            logging.debug(f"SetBucketAttributes response: {response}")
+            return response
+
+    def GetObjectAttributes(self, request, context):
+        with tracer.start_as_current_span("GetObjectAttributes"):
+            logging.debug(f"GetObjectAttributes request: [{request}]")
+
+            bucket = self.get_bucket(
+                request.bucket_name, request.bucket_id, args.create_bucket_if_missing)
+            if bucket is None:
+                context.set_code(grpc.StatusCode.NOT_FOUND)
+                context.set_details("Bucket not found")
+                logging.error(
+                    f"Bucket not found: {request.bucket_name} / {request.bucket_id}: "
+                    f"{context.code()} {context.details().decode('UTF-8')}")
+                return mdoffload_pb2.GetObjectAttributesResponse()
+
+            try:
+                obj = self.get_object(
+                    bucket, request.object_key, request.object_instance_id, args.create_object_if_missing)
+                if obj is None:
+                    context.set_code(grpc.StatusCode.NOT_FOUND)
+                    context.set_details("Object not found")
+                    return mdoffload_pb2.GetObjectAttributesResponse()
+            except ValueError as ve:
+                context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+                context.set_details(str(ve))
+                logging.error(f"Invalid argument: {request.bucket_name} / {request.bucket_id} / "
+                              f"{request.object_key} / {request.object_instance_id}: "
+                              f"{context.code()} {context.details().decode('UTF-8')}")
+                return mdoffload_pb2.GetObjectAttributesResponse()
+
+            response = mdoffload_pb2.GetObjectAttributesResponse(
+                attributes=obj.attrs().list()
+            )
+            logging.debug(f"GetObjectAttributes response: {response}")
+            return response
+
+    def SetObjectAttributes(self, request, context):
+        with tracer.start_as_current_span("SetObjectAttributes"):
+            logging.debug(f"SetObjectAttributes request: [{request}]")
+
+            bucket = self.get_bucket(
+                request.bucket_name, request.bucket_id, args.create_bucket_if_missing)
+            if bucket is None:
+                context.set_code(grpc.StatusCode.NOT_FOUND)
+                context.set_details("Bucket not found")
+                logging.error(
+                    f"Bucket not found: {request.bucket_name} / {request.bucket_id}: {context.code()} {context.details()}")
+                return mdoffload_pb2.SetObjectAttributesResponse()
+            obj = self.get_object(
+                bucket, request.object_key, request.object_instance_id, args.create_object_if_missing)
+            if obj is None:
+                context.set_code(grpc.StatusCode.NOT_FOUND)
+                context.set_details("Object not found")
+                return mdoffload_pb2.SetObjectAttributesResponse()
+            obj.attrs().update(request.attributes_to_add, request.attributes_to_delete)
+            logging.debug(
+                f"Updated object attributes: {','.join([f'{k}={v}' for k,v in obj.attrs().list()])}")
+
+            response = mdoffload_pb2.SetObjectAttributesResponse()
+
+            logging.debug(f"SetObjectAttributes response: {response}")
+            return response
+
+
+def _load_credential_from_file(filepath):
+    """https://github.com/grpc/grpc/blob/master/examples/python/auth/_credentials.py"""
+    real_path = os.path.join(os.path.dirname(__file__), filepath)
+    with open(real_path, "rb") as f:
+        return f.read()
+
+
+def run(args):
+    server_address = f"127.0.0.1:{args.port}"
+    logging.info("Starting gRPC service...\n")
+    try:
+        server = grpc.server(
+            futures.ThreadPoolExecutor(max_workers=10),
+            options=(
+                ("grpc.so_reuseport", 0),
+            ),  # This apparently helps detect port reuse - see https://github.com/grpc/grpc/issues/16920
+        )
+        mdoffload_pb2_grpc.add_MDOffloadServiceServicer_to_server(
+            MDOffloadServer(args), server
+        )
+
+        if args.tls:
+            server_crt = _load_credential_from_file(args.server_cert)
+            server_key = _load_credential_from_file(args.server_key)
+            server_credentials = grpc.ssl_server_credentials(
+                (
+                    (
+                        server_key,
+                        server_crt,
+                    ),
+                )
+            )
+            server.add_secure_port(server_address, server_credentials)
+
+        else:
+            server.add_insecure_port(server_address)
+
+        server.start()
+        logging.info(f"Server started, listening on {server_address}")
+        server.wait_for_termination()
+    except KeyboardInterrupt:
+        pass
+    logging.info("Stopping gRPC server...\n")
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    p = argparse.ArgumentParser(description="Authorizer gRPC server")
+    p.add_argument("-B", "--create-bucket-if-missing",
+                   help="always create the bucket if it does not exist",
+                   action="store_true")
+    p.add_argument("-O", "--create-object-if-missing",
+                   help="always create the object if it does not exist",
+                   action="store_true")
+    p.add_argument("port", type=int, help="Listen port",
+                   nargs="?", default=8004)
+    p.add_argument(
+        "-t", "--tls", help="connect to the server using TLS", action="store_true"
+    )
+    p.add_argument("-v", "--verbose", action="store_true",
+                   help="Enable verbose output")
+    ptls = p.add_argument_group("TLS arguments")
+    ptls.add_argument("--ca-cert", help="CA certificate file (NOT YET USED)")
+    ptls.add_argument("--server-cert", help="client certificate file")
+    ptls.add_argument("--server-key", help="client key file")
+
+    args = p.parse_args()
+    if args.verbose:
+        coloredlogs.install(level=logging.DEBUG, isatty=True)
+    else:
+        coloredlogs.install(level=logging.INFO, isatty=True)
+
+    if args.tls:
+        if not args.server_cert:
+            logging.error("TLS requires a server certificate")
+            sys.exit(1)
+        if not args.server_key:
+            logging.error("TLS requires a server key")
+            sys.exit(1)
+
+    run(args)
